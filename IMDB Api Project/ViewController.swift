5d48256f34dd6566c05ffb4e1947e8aab31588e2
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2017-02-12 20:23:08 +0300

    final version

--- IMDB Api Project/ViewController.swift
+++ IMDB Api Project/ViewController.swift
@@ -8,18 +8,89 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var directorLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var actorsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        searchBar.delegate = self
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        searchForMovie(title: searchBar.text!)
+        searchBar.text = ""
+        
     }
+    
+    func searchForMovie(title: String) {
+        
+        if let movie = title.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) {
+            
+            let url = URL(string: "http://www.omdbapi.com/?t=\(movie)")
+            
+            let session = URLSession.shared
+            
+            let task = session.dataTask(with: url!, completionHandler: { (data, response, error) in
+                
+                if error != nil {
+                    let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertControllerStyle.alert)
+                    let ok = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+                    alert.addAction(ok)
+                    self.present(alert, animated: true, completion: nil)
+                } else {
+                    
+                    if data != nil {
+                        
+                        do {
+                            
+                            let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String,String>
+                            
+                            DispatchQueue.main.async {
+                                
+                                self.titleLabel.text = jsonResult["Title"]
+                                self.ratingLabel.text = jsonResult["imdbRating"]
+                                self.directorLabel.text = jsonResult["Director"]
+                                self.actorsLabel.text = jsonResult["Actors"]
+                                
+                                if let imageExists = jsonResult["Poster"] {
+                                    
+                                    let imageURL = URL(string: imageExists)
+                                    
+                                    if let imageData = try? Data(contentsOf: imageURL!) {
+                                        self.imageView.image = UIImage(data: imageData)
+                                    }
+                                }
+                                
+                            }
+                            
+                        } catch {
+                            
+                        }
+                        
+                        
+                    }
+                    
+                }
+                
+            })
+            
+            task.resume()
+            
+        }
+        
+        
+        
+    }
+    
 
-
-}
+   }
 
